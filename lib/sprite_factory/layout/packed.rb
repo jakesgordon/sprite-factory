a0c84f52d5e7f8da7b697d01148a74e15fbb489f
Jake Gordon <jake@codeincomplete.com>
2011-05-08 14:43:40 -0700

    rough draft of working packed algorithm (ported from javascript http://codeincomplete.com/posts/2011/5/7/bin_packing/example/)
     - needs polish and more unit tests

--- lib/sprite_factory/layout/packed.rb
+++ lib/sprite_factory/layout/packed.rb
@@ -3,9 +3,102 @@ module SpriteFactory
     module Packed
 
       def self.layout(images, options = {})
-        raise NotImplementedError, "COMING SOON: http://codeincomplete.com/posts/2011/5/7/bin_packing/"
+
+        return { :width => 0, :height => 0 } if images.empty?
+
+        images.sort! do |a,b|
+          diff = [b[:width], b[:height]].max <=> [a[:width], b[:height]].max
+          diff = [b[:width], b[:height]].min <=> [a[:width], b[:height]].min if diff.zero?
+          diff = b[:height] <=> a[:height] if diff.zero?
+          diff = b[:width]  <=> a[:width]  if diff.zero?
+          diff
+        end
+
+        root = { :x => 0, :y => 0, :w => images[0][:width], :h => images[0][:height] }
+
+        images.each do |i|
+          if (node = findNode(root, i[:width], i[:height]))
+            placeImage(i, node)
+            splitNode(node, i[:width], i[:height])
+          else
+            root = grow(root, i[:width], i[:height])
+            redo
+          end
+        end
+
+        { :width => root[:w], :height => root[:h] }
+
       end
 
-    end
-  end
-end
+      def self.placeImage(image, node)
+        # puts "placing image #{image[:width]}x#{image[:height]} at #{node[:x]}x#{node[:y]}"
+        image[:x] = image[:cssx] = node[:x]
+        image[:y] = image[:cssy] = node[:y]
+        image[:cssw] = image[:width]
+        image[:cssh] = image[:height]
+        # TODO: add support for :padding and fixed :width/:height
+      end
+
+      def self.findNode(root, w, h)
+        if root[:used]
+          findNode(root[:right], w, h) || findNode(root[:down], w, h)
+        elsif (w <= root[:w]) && (h <= root[:h])
+          root
+        end
+      end
+
+      def self.splitNode(node, w, h)
+        node[:used]  = true
+        node[:down]  = { :x => node[:x],     :y => node[:y] + h, :w => node[:w],     :h => node[:h] - h }
+        node[:right] = { :x => node[:x] + w, :y => node[:y],     :w => node[:w] - w, :h => h            }
+      end
+
+      def self.grow(root, w, h)
+
+        canGrowDown  = (w <= root[:w])
+        canGrowRight = (h <= root[:h])
+
+        shouldGrowRight = canGrowRight && (root[:h] >= (root[:w] + w))
+        shouldGrowDown  = canGrowDown  && (root[:w] >= (root[:h] + h))
+
+        if shouldGrowRight
+          growRight(root, w, h)
+        elsif shouldGrowDown
+          growDown(root, w, h)
+        elsif canGrowRight
+          growRight(root, w, h)
+        elsif canGrowDown
+          growDown(root, w, h)
+        else
+          raise RuntimeError, "can't fit #{w}x#{h} block into root #{root[:w]}x#{root[:h]} - this should not happen if images are pre-sorted correctly"
+        end
+
+      end
+
+      def self.growRight(root, w, h)
+        return {
+          :used  => true,
+          :x     => 0,
+          :y     => 0,
+          :w     => root[:w] + w,
+          :h     => root[:h],
+          :down  => root,
+          :right => { :x => root[:w], :y => 0, :w => w, :h => root[:h] }
+        }
+      end
+
+      def self.growDown(root, w, h)
+        return {
+          :used  => true,
+          :x     => 0,
+          :y     => 0,
+          :w     => root[:w],
+          :h     => root[:h] + h,
+          :down  => { :x => 0, :y => root[:h], :w => root[:w], :h => h },
+          :right => root
+        }
+      end
+
+      end # module Packed
+  end # module Layout
+end # module SpriteFactory
--- test/layout/packed_test.rb
+++ test/layout/packed_test.rb
@@ -9,10 +9,28 @@ module SpriteFactory
     #==========================================================================
 
     def test_packed_layout_of_regular_images
+      images = get_regular_images
+      expected = [                    # expected:  ---------
+        { :x =>  0, :y =>  0 },       #            | 1 | 3 |
+        { :x =>  0, :y => 10 },       #            ---------
+        { :x => 20, :y =>  0 },       #            | 2 | 4 |
+        { :x => 20, :y => 10 },       #            ---------
+        { :x =>  0, :y => 20 }        #            | 5 |   |
+      ]                               #            ---------
+      verify_layout(40, 30, expected, images, :layout => :packed)
+    end
+
+    #--------------------------------------------------------------------------
+
+    def test_padded_packed_layout_of_regular_images
       # TODO
     end
 
     #--------------------------------------------------------------------------
+
+    def test_fixed_packed_layout_of_regular_images
+      # TODO
+    end 
 
     #==========================================================================
     # test IRREGULAR images
@@ -20,6 +38,61 @@ module SpriteFactory
 
     def test_packed_layout_of_irregular_images
       # TODO
+    end
+
+    #--------------------------------------------------------------------------
+
+    def test_padded_packed_layout_of_irregular_images
+      # TODO
+    end
+
+    #--------------------------------------------------------------------------
+
+    def test_fixed_packed_layout_of_irregular_images
+      # TODO
+    end
+
+    #==========================================================================
+    # other packed algorithm test
+    #==========================================================================
+
+    def test_single_image_is_100_percent_packed
+      images = [
+        { :width => 100, :height => 100 }
+      ]
+      expected = [
+        { :x => 0, :y => 0 }
+      ]
+      verify_layout(100, 100, expected, images, :layout => :packed)
+    end
+
+    #==========================================================================
+    # test cases from original bin packing demonstration
+    #   (see http://codeincomplete.com/posts/2011/5/7/bin_packing/example/)
+    #==========================================================================
+
+    def test_packed_simple
+    end
+
+    def test_packed_square
+    end
+
+    def test_packed_tall
+    end
+
+    def test_packed_wide
+    end
+
+    def test_packed_tall_and_wide
+    end
+
+    def test_packed_powers_of_2
+    end
+
+    def test_packed_odd_and_even
+    end
+
+    def test_complex
     end
 
     #--------------------------------------------------------------------------
